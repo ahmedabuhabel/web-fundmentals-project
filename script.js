d99b7a269f4dd3fb0c17c66c078ded92624b8b39
BaraaSalah <baraasalahbs96@gmail.com>
2026-04-04 12:15:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let signIn = document.getElementById("signIn");
+signIn.onmousemove= function(){
+    document.body.style.backgroundColor=" red";
+}
+signIn.onmouseout= function(){
+    document.body.style.backgroundColor="gray"
+}
